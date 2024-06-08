2351adef18daf1f432aa25173047b1bd8694cfd9
Patrick Taylor <hello@patricktaylor.dev>
2024-06-09 00:08:20 +0100

    Log a customiseable title when running lint-pilot 💙

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 import { Command } from 'commander'
 
+import chalk from 'chalk'
+
 const program = new Command()
 
 program
+  .name('lint-pilot')
+  .description('Lint Pilot: Your co-pilot for maintaining high code quality with seamless ESLint and Stylelint integration.')
   .version('0.0.1')
-  .description('Lint Pilot')
 
 program
   .command('greet')
@@ -15,8 +18,11 @@ program
   })
 
 program
-  .action(() => {
-    console.log('Running LintPilot with default settings.')
+  .option('-t, --title <string>', 'customise the title displayed when running lint-pilot')
+  .action(options => {
+    console.clear()
+    console.log(chalk.cyan(options.title ? options.title : '🛫 Lint Pilot 🛬'))
+    console.log()
   })
 
 program.parse(process.argv)
