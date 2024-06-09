b6a3f9402659466bddcdfd65b2a4afa0a788a837
Patrick Taylor <hello@patricktaylor.dev>
2024-06-09 19:32:34 +0100

    Updated project description ℹ️

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const program = new Command()
 
 program
   .name('lint-pilot')
-  .description('Lint Pilot: Your co-pilot for maintaining high code quality with seamless ESLint and Stylelint integration.')
+  .description('Lint Pilot: Your co-pilot for maintaining high code quality with seamless ESLint, Stylelint, and MarkdownLint integration.')
   .version('0.0.1')
 
 /*
