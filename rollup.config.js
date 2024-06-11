edf507fc0d7a9cf4bb5db68ad725e1a25375decd
Patrick Taylor <hello@patricktaylor.dev>
2024-06-11 18:24:06 +0100

    Set rollup externals as package.json dependencies 🏭

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,12 @@
+import { readFileSync } from 'fs'
+import { resolve } from 'path'
+
 import { terser } from 'rollup-plugin-terser'
 
+const packageJSON = JSON.parse(readFileSync(resolve('./package.json'), 'utf-8'))
+
 export default {
+  external: Object.keys(packageJSON.dependencies),
   input: 'src/index.js',
   output: {
     file: 'lib/index.min.js',
