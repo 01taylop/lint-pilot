dbf365915ca1a24608d86e50449d9c494398cfa7
Patrick Taylor <hello@patricktaylor.dev>
2024-06-21 23:39:31 +0100

    Added eslint-plugin-import to the config 🍭

--- config/eslint/build-config.ts
+++ config/eslint/build-config.ts
@@ -1,10 +1,13 @@
+import ImportRules from './rules/import'
 import InclusiveLanguageRules from './rules/inclusive-language'
 
 const buildConfig = () => ({
   plugins: [
+    'import',
     'inclusive-language',
   ],
   rules: {
+    ...ImportRules,
     ...InclusiveLanguageRules,
   },
 })
