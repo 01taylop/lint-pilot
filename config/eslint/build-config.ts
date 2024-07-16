404dde03beaabc44c608d5ee834c18bfb3f9db29
Patrick Taylor <hello@patricktaylor.dev>
2024-07-16 12:49:35 +0100

    Configured `eslint-plugin-sort-destructure-keys` 🔑 (#7)
    * Added eslint-plugin-sort-destructure-keys 🔑
    
    * Updated depcheck ☑️

--- config/eslint/build-config.ts
+++ config/eslint/build-config.ts
@@ -1,11 +1,14 @@
 import InclusiveLanguageRules from './rules/inclusive-language'
+import SortDestructureKeysRules from './rules/sort-destructure-keys'
 
 const buildConfig = () => ({
   plugins: [
     'inclusive-language',
+    'sort-destructure-keys',
   ],
   rules: {
     ...InclusiveLanguageRules,
+    ...SortDestructureKeysRules,
   },
 })
 
