04b008d5de6ceaf745787a62b34b9accfccef40c
Patrick Taylor <hello@patricktaylor.dev>
2024-07-17 14:05:07 +0100

    Improved markdownlint tests 🪭

--- src/linters/markdownlint/__tests__/index.spec.ts
+++ src/linters/markdownlint/__tests__/index.spec.ts
@@ -88,11 +88,10 @@ describe('markdownlint', () => {
     })
   })
 
-  describe('when markdownlint successfully lints (with errors)', () => {
+  it('resolves with results and a summary when markdownlint successfully lints (with errors)', async () => {
 
     const commonError = {
-      ruleNames: ['MD000', 'test-rule'],
-      ruleDescription: 'test-rule-description',
+      ruleNames: ['MD000', 'test-rule-name'],
       ruleInformation: 'test-rule-information',
       errorDetail: 'test-error-detail',
       errorContext: 'test-error-context',
@@ -106,6 +105,7 @@ describe('markdownlint', () => {
         fixInfo: {
           lineNumber: 1,
         },
+        ruleDescription: 'test-rule-description',
       }],
       'CONTRIBUTING.md': [],
       'README.md': [{
@@ -115,93 +115,92 @@ describe('markdownlint', () => {
         fixInfo: {
           lineNumber: 7,
         },
+        ruleDescription: 'no-error-range',
       }, {
         ...commonError,
         errorDetail: '',
-        lineNumber: 18,
+        lineNumber: 9,
+        ruleDescription: 'no-error-detail',
       }, {
         ...commonError,
         lineNumber: 13,
         ruleNames: ['MD000', 'test-rule-b'],
+        ruleDescription: 'sorted-by-name',
       }, {
         ...commonError,
         lineNumber: 13,
         ruleNames: ['MD000', 'test-rule-a'],
+        ruleDescription: 'sorted-by-name',
+      }, {
+        ...commonError,
+        errorDetail: '',
+        lineNumber: 3,
+        ruleDescription: 'sort-by-line-number',
       }],
     }
 
-    it('formats the errors sorted by lineNumber and then alphabetically by ruleName', async () => {
-      jest.mocked(markdownlint).mockImplementationOnce((_options, callback) => {
-        callback(null, markdownlintResult)
-      })
+    const commonResult = {
+      messageTheme: expect.any(Function),
+      positionTheme: expect.any(Function),
+      ruleTheme: expect.any(Function),
+    }
 
-      expect(await markdownLib.lintFiles(testFiles)).toStrictEqual({
-        results: {
-          'CHANGELOG.md': [{
-            message: 'test-rule-description: test-error-detail',
-            messageTheme: expect.any(Function),
-            position: '1:1',
-            positionTheme: expect.any(Function),
-            rule: 'test-rule',
-            ruleTheme: expect.any(Function),
-            severity: 'X',
-          }],
-          'README.md': [{
-            message: 'test-rule-description: test-error-detail',
-            messageTheme: expect.any(Function),
-            position: '7',
-            positionTheme: expect.any(Function),
-            rule: 'test-rule',
-            ruleTheme: expect.any(Function),
-            severity: 'X',
-          }, {
-            message: 'test-rule-description: test-error-detail',
-            messageTheme: expect.any(Function),
-            position: '13:1',
-            positionTheme: expect.any(Function),
-            rule: 'test-rule-a',
-            ruleTheme: expect.any(Function),
-            severity: 'X',
-          }, {
-            message: 'test-rule-description: test-error-detail',
-            messageTheme: expect.any(Function),
-            position: '13:1',
-            positionTheme: expect.any(Function),
-            rule: 'test-rule-b',
-            ruleTheme: expect.any(Function),
-            severity: 'X',
-          }, {
-            message: 'test-rule-description',
-            messageTheme: expect.any(Function),
-            position: '18:1',
-            positionTheme: expect.any(Function),
-            rule: 'test-rule',
-            ruleTheme: expect.any(Function),
-            severity: 'X',
-          }],
-        },
-        summary: expect.any(Object),
-      })
+    jest.mocked(markdownlint).mockImplementationOnce((_options, callback) => {
+      callback(null, markdownlintResult)
     })
 
-    it('counts fixable errors correctly', async () => {
-      jest.mocked(markdownlint).mockImplementationOnce((_options, callback) => {
-        callback(null, markdownlintResult)
-      })
-
-      expect(await markdownLib.lintFiles(testFiles)).toStrictEqual({
-        results: expect.any(Object),
-        summary: {
-          deprecatedRules: [],
-          errorCount: 5,
-          fileCount: 3,
-          fixableErrorCount: 2,
-          fixableWarningCount: 0,
-          linter: 'MarkdownLint',
-          warningCount: 0,
-        },
-      })
+    expect(await markdownLib.lintFiles(testFiles)).toStrictEqual({
+      results: {
+        'CHANGELOG.md': [{
+          ...commonResult,
+          message: 'test-rule-description: test-error-detail',
+          position: '1:1',
+          rule: 'test-rule-name',
+          severity: 'X',
+        }],
+        'README.md': [{
+          ...commonResult,
+          message: 'sort-by-line-number',
+          position: '3:1',
+          rule: 'test-rule-name',
+          severity: 'X',
+        }, {
+          ...commonResult,
+          message: 'no-error-range: test-error-detail',
+          position: '7',
+          rule: 'test-rule-name',
+          severity: 'X',
+        }, {
+          ...commonResult,
+          message: 'no-error-detail',
+          position: '9:1',
+          rule: 'test-rule-name',
+          severity: 'X',
+        }, {
+          ...commonResult,
+          message: 'sorted-by-name: test-error-detail',
+          position: '13:1',
+          rule: 'test-rule-a',
+          severity: 'X',
+        }, {
+          ...commonResult,
+          message: 'sorted-by-name: test-error-detail',
+          position: '13:1',
+          rule: 'test-rule-b',
+          severity: 'X',
+        }],
+      },
+      summary: {
+        deprecatedRules: [],
+        errorCount: 6,
+        fileCount: 3,
+        fixableErrorCount: 2,
+        fixableWarningCount: 0,
+        linter: 'MarkdownLint',
+        warningCount: 0,
+      },
     })
+
   })
 
 })
