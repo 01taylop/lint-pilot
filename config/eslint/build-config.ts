25334eaeb34b2206a0efe3432d5a3f664591069f
Patrick Taylor <hello@patricktaylor.dev>
2024-07-17 14:39:34 +0100

    Configured `eslint-plugin-eslint-comments` 💬 (#10)

--- config/eslint/build-config.ts
+++ config/eslint/build-config.ts
@@ -1,14 +1,17 @@
+import ESLintComments from './rules/eslint-comments'
 import InclusiveLanguage from './rules/inclusive-language'
 import SortDestructureKeys from './rules/sort-destructure-keys'
 import SortExports from './rules/sort-exports'
 
 const buildConfig = () => ({
   plugins: [
+    'eslint-comments',
     'inclusive-language',
     'sort-destructure-keys',
     'sort-exports',
   ],
   rules: {
+    ...ESLintComments,
     ...InclusiveLanguage,
     ...SortDestructureKeys,
     ...SortExports,
