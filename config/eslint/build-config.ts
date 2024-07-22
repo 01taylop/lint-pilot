cb2723fce123bbfa50d31d170fcb9a36752288c5
Patrick Taylor <hello@patricktaylor.dev>
2024-07-22 23:56:19 +0100

    Configured `eslint-plugin-jest` 🃏

--- config/eslint/build-config.ts
+++ config/eslint/build-config.ts
@@ -1,10 +1,14 @@
 import ESLintComments from './rules/eslint-comments'
 import InclusiveLanguage from './rules/inclusive-language'
+import { JestRules, JestTypescriptRules } from './rules/jest'
 import PromiseRules from './rules/promise'
 import SortDestructureKeys from './rules/sort-destructure-keys'
 import SortExports from './rules/sort-exports'
 
 const buildConfig = () => ({
+  env: {
+    'jest/globals': true,
+  },
   plugins: [
     'eslint-comments',
     'inclusive-language',
@@ -19,6 +23,20 @@ const buildConfig = () => ({
     ...SortDestructureKeys,
     ...SortExports,
   },
+  overrides: [{
+    files: ['*.spec.*', '*.test.*'],
+    plugins: [
+      'jest',
+    ],
+    rules: {
+      ...JestRules,
+    },
+  }, {
+    files: ['*.spec.ts', '*.spec.tsx', '*.test.ts', '*.test.tsx'],
+    rules: {
+      ...JestTypescriptRules,
+    },
+  }],
 })
 
 export default buildConfig
