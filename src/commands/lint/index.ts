f3c0df8653b48b752032d54ff363c522542877fb
Patrick Taylor <hello@patricktaylor.dev>
2024-10-27 22:07:51 +0000

    Clear the cache when requested in the lint command 🔮

--- src/commands/lint/index.ts
+++ src/commands/lint/index.ts
@@ -1,10 +1,17 @@
 import type { LintOptions } from '@Types/commands'
+import { clearCacheDirectory } from '@Utils/cache'
 import colourLog from '@Utils/colour-log'
 import { clearTerminal } from '@Utils/terminal'
 
-const lint = ({ emoji, title, ...options }: LintOptions) => {
+const lint = ({ clearCache, debug, emoji, title, ...options }: LintOptions) => {
+  global.debug = debug
+
   clearTerminal()
   colourLog.title(`${emoji} ${title}\n`)
+
+  if (clearCache) {
+    clearCacheDirectory()
+  }
 
   console.log('Run Lint', options)
 }
