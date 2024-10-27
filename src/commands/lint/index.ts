b8ea88dcf9336eaecc9825d9239bae5e97c35091
Patrick Taylor <hello@patricktaylor.dev>
2024-10-27 21:52:03 +0000

    Clear terminal when running lint command 🆑

--- src/commands/lint/index.ts
+++ src/commands/lint/index.ts
@@ -1,6 +1,9 @@
 import type { LintOptions } from '@Types/commands'
+import { clearTerminal } from '@Utils/terminal'
 
 const lint = (options: LintOptions) => {
+  clearTerminal()
+
   console.log('Run Lint', options)
 }
 
