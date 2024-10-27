79e160671a5c930959f0a04bd86a3c621ae94b6c
Patrick Taylor <hello@patricktaylor.dev>
2024-10-27 20:59:14 +0000

    Prepare the lint command 📡

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,16 +1,21 @@
 import chalk from 'chalk'
 
 import { description, name, version } from '../../package.json'
-
-import program from '../index'
+import { lint } from '../commands'
+import createProgram from '../index'
 
 jest.mock('chalk', () => ({
   red: jest.fn(text => text),
 }))
 
+jest.mock('../commands', () => ({
+  lint: jest.fn(),
+}))
+
 describe('lint-pilot', () => {
 
   it('displays the help text', () => {
+    const program = createProgram()
     const helpInformation = program
       .configureHelp({
         helpWidth: -1,
@@ -26,18 +31,19 @@ Options:
   -h, --help      display help for command
 
 Commands:
-  lint            run all linters: ESLint, Stylelint, and MarkdownLint (default)
+  lint [options]  run all linters: ESLint, Stylelint, and MarkdownLint (default)
   help [command]  display help for command
 `)
   })
 
   it('displays the version', () => {
+    const program = createProgram()
     program
       .configureOutput({ writeOut: jest.fn() })
       .exitOverride()
 
     expect(() => {
-      program.parse(['node', './index.ts', '--version']);
+      program.parse(['node', './index.ts', '--version'])
     }).toThrow(version)
   })
 
@@ -46,13 +52,14 @@ Commands:
 
     const writeErrMock = jest.fn()
 
+    const program = createProgram()
     program
       .configureOutput({
         writeErr: writeErrMock,
       })
 
     try {
-      program.parse(['node', './index.ts', '--unknown']);
+      program.parse(['node', './index.ts', '--unknown'])
     } catch (e: any) {
       expect(e.message).toMatch('process.exit(1)')
     }
@@ -62,4 +69,45 @@ Commands:
     expect(writeErrMock).toHaveBeenNthCalledWith(2, expect.stringContaining('💡 Run `lint-pilot --help` for more information.'))
   })
 
+  it('calls the lint command by default', () => {
+    const program = createProgram()
+    program.parse(['node', './index.ts'])
+
+    expect(lint).toHaveBeenCalledTimes(1)
+  })
+
+  it('calls the lint command with default options', () => {
+    const program = createProgram()
+    program.parse(['node', './index.ts', 'lint'])
+
+    expect(lint).toHaveBeenCalledOnceWith({
+      cache: false,
+      clearCache: false,
+      debug: false,
+      emoji: '✈️',
+      eslintUseLegacyConfig: false,
+      fix: false,
+      title: 'Lint Pilot',
+      watch: false,
+    })
+  })
+
+  it('calls the lint command with configured options', () => {
+    const program = createProgram()
+    program.parse(['node', './index.ts', 'lint', '--emoji', '🚀', '--title', 'Rocket Lint', '--fix', '--watch', '--cache', '--clearCache', '--ignore-dirs', 'node_modules', '--ignore-patterns', 'dist', '--eslint-include', '*.mdx', '--debug', '--eslint-use-legacy-config'])
+
+    expect(lint).toHaveBeenCalledOnceWith({
+      cache: true,
+      clearCache: true,
+      debug: true,
+      emoji: '🚀',
+      eslintInclude: ['*.mdx'],
+      eslintUseLegacyConfig: true,
+      fix: true,
+      ignoreDirs: ['node_modules'],
+      ignorePatterns: ['dist'],
+      title: 'Rocket Lint',
+      watch: true,
+    })
+  })
 })
