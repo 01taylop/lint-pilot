c99a57154d890a568205d640eb2d8f0243a8bc5d
Patrick Taylor <hello@patricktaylor.dev>
2025-05-04 21:08:49 +0100

    Improved logging in the exitHandler function 🪵

--- src/exitHandler.ts
+++ src/exitHandler.ts
@@ -1,12 +1,27 @@
 import chalk from 'chalk'
 
-const exitHandler = (code: number, error?: string, reason?: unknown) => {
-  if (error) {
-    console.error(chalk.red(`\n× ${error}`))
-    if (reason) {
-      console.error(chalk.white(reason))
+const logReason = (reason: string) => console.error(`\n${chalk.white(reason)}`)
+
+const exitHandler = (code: 0 | 1, error: string, reason?: Error | string | unknown) => {
+  console.error(chalk.red(`\n× ${error}`))
+
+  if (reason) {
+    if (reason instanceof Error) {
+      logReason(reason.message)
+      if (reason.stack) {
+        console.error(`\n${chalk.gray(reason.stack)}`)
+      }
+    } else if (typeof reason === 'string') {
+      logReason(reason)
+    } else {
+      try {
+        logReason(JSON.stringify(reason))
+      } catch {
+        logReason('Unable to stringify reason')
+      }
     }
   }
+
   process.exit(code)
 }
 
