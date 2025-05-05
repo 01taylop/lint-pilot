ee66fb924134abc3c88c1d085bd14ac866f8af93
Patrick Taylor <hello@patricktaylor.dev>
2025-05-05 20:57:34 +0100

    Refactored runLinter command 🧼

---
+++ src/linters/linters.ts
@@ -0,0 +1,21 @@
+import eslint from './eslint'
+import markdownlint from './markdownlint'
+import stylelint from './stylelint'
+
+import { Linter } from '@Types/lint'
+
+import type { LintFilesOptions, LintReport } from '@Types/lint'
+
+type Linters = {
+  [key in Linter]: {
+    lintFiles: (options: LintFilesOptions) => Promise<LintReport>
+  }
+}
+
+const linters: Linters = {
+  [Linter.ESLint]: eslint,
+  [Linter.Markdownlint]: markdownlint,
+  [Linter.Stylelint]: stylelint,
+}
+
+export default linters
--- src/types/lint.ts
+++ src/types/lint.ts
@@ -1,8 +1,16 @@
+/*
+ * ENUMS
+ */
+
 enum Linter {
   ESLint = 'ESLint',
   Markdownlint = 'Markdownlint',
   Stylelint = 'Stylelint',
 }
+
+/*
+ * LINTING
+ */
 
 interface FilePatterns {
   includePatterns: {
@@ -11,10 +19,61 @@ interface FilePatterns {
   ignorePatterns: Array<string>
 }
 
+interface LintFilesOptions {
+  cache: boolean
+  eslintUseLegacyConfig?: boolean
+  files: Array<string>
+  fix: boolean
+}
+
+/*
+ * REPORTING
+ */
+
+enum RuleSeverity {
+  ERROR = 'error',
+  WARNING = 'warning',
+}
+
+interface FormattedResult {
+  message: string
+  messageTheme: Function
+  position: string
+  positionTheme: Function
+  rule: string
+  ruleTheme: Function
+  severity: string
+}
+
+interface ReportResults {
+  [file: string]: Array<FormattedResult>
+}
+
+interface ReportSummary {
+  deprecatedRules: Array<string>
+  errorCount: number
+  fileCount: number
+  fixableErrorCount: number
+  fixableWarningCount: number
+  linter: Linter
+  warningCount: number
+}
+
+interface LintReport {
+  results: ReportResults
+  summary: ReportSummary
+}
+
 export type {
   FilePatterns,
+  FormattedResult,
+  LintFilesOptions,
+  LintReport,
+  ReportResults,
+  ReportSummary,
 }
 
 export {
   Linter,
+  RuleSeverity,
 }
