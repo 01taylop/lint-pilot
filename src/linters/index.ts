fb11f2e3d7dd07ad555aa85e608c819eed7b21db
Patrick Taylor <hello@patricktaylor.dev>
2025-05-05 18:45:08 +0100

    Updated promise functionality to run specified linters 💍

--- src/linters/index.ts
+++ src/linters/index.ts
@@ -1,13 +1,40 @@
+import { Linter } from '@Types/lint'
+
 import type { LintCommandOptions } from '@Types/commands'
-import type { FilePatterns, Linter } from '@Types/lint'
+import type { FilePatterns } from '@Types/lint'
+
+import runLinter from './run-linter'
 
 type RunLintersOptions = Pick<LintCommandOptions, 'cache' | 'eslintUseLegacyConfig' | 'fix' | 'title' | 'watch'> & {
   filePatterns: FilePatterns
   linters?: Array<Linter>
 }
 
-const runLinters = ({ cache, eslintUseLegacyConfig, filePatterns, fix, linters, title, watch }: RunLintersOptions) => {
-  console.log('Running linters...')
+const runLinters = async ({ cache, eslintUseLegacyConfig, filePatterns, fix, linters, title, watch }: RunLintersOptions) => {
+  const commonArgs = {
+    cache,
+    eslintUseLegacyConfig,
+    fix,
+    ignore: filePatterns.ignorePatterns,
+  }
+
+  const lintPromises = Object.values(Linter)
+    .filter(linter => !linters || linters.includes(linter))
+    .map(linter => runLinter({
+      ...commonArgs,
+      filePattern: filePatterns.includePatterns[linter],
+      linter,
+    }))
+
+  return Promise.allSettled(lintPromises).then(results => {
+    results.forEach((result) => {
+      if (result.status === 'fulfilled') {
+        console.log(`Linter completed successfully:`, result.value)
+      } else {
+        console.error(`Linter failed:`, result.reason)
+      }
+    })
+  })
 }
 
 export default runLinters
