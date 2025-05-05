7b95f0c8071d4edf17ee40db646ef45ff943c71d
Patrick Taylor <hello@patricktaylor.dev>
2025-05-05 01:02:40 +0100

    Moved shebang and process events to the index file 💥

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
+#!/usr/bin/env node
+import { exitHandler } from './exitHandler'
 import { createProgram } from './program'
+
+process.on('exit', () => console.log())
+process.on('SIGINT', () => exitHandler(0, 'SIGINT'))
+process.on('SIGTERM', () => exitHandler(0, 'SIGTERM'))
+process.on('uncaughtException', reason => exitHandler(1, 'Unexpected Error', reason))
+process.on('unhandledRejection', reason => exitHandler(1, 'Unhandled Promise', reason))
 
 const program = createProgram()
 
