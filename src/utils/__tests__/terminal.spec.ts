92dca1fe6099e37449003f156f9bddb6672a6b3b
Patrick Taylor <hello@patricktaylor.dev>
2025-06-02 11:41:08 +0100

    Minor util improvements ✨

--- src/utils/__tests__/terminal.spec.ts
+++ src/utils/__tests__/terminal.spec.ts
@@ -3,11 +3,11 @@ import { clearTerminal } from '../terminal'
 describe('clearTerminal', () => {
 
   it('calls process.stdout.write to clear the terminal', () => {
-    const mockWrite = jest.spyOn(process.stdout, 'write').mockImplementation(() => true)
+    const stdoutSpy = jest.spyOn(process.stdout, 'write').mockImplementation(() => true)
 
     clearTerminal()
 
-    expect(mockWrite).toHaveBeenCalledWith('\x1Bc\x1B[3J\x1B[2J\x1B[H')
+    expect(stdoutSpy).toHaveBeenCalledWith('\x1Bc\x1B[3J\x1B[2J\x1B[H')
   })
 
 })
--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -1,3 +1,4 @@
+// Simple pluralisation by adding 's', does not handle irregular plurals.
 const pluralise = (word: string, count: number) => count === 1 ? word : `${word}s`
 
 export {
