a7127ba4d83bf50a5a1b122145f384a9a5ee4d55
Patrick Taylor <hello@patricktaylor.dev>
2025-07-21 20:03:16 +0100

    Simplified `toHaveBeenCalledOnceWith` Jest matcher 🤡

--- jest-config/setup.ts
+++ jest-config/setup.ts
@@ -20,19 +20,15 @@ beforeEach(() => {
 
 expect.extend({
   toHaveBeenCalledOnceWith(received, ...expected) {
-    const isCalledOnce = received.mock.calls.length === 1
-    let isCalledWithExpected = false
-
-    if (isCalledOnce) {
-      isCalledWithExpected = expected.every((arg, index) => this.equals(received.mock.calls[0][index], arg))
-    }
+    const calls = received.mock.calls
+    const pass = calls.length === 1 && expected.every((arg, index) => this.equals(calls[0][index], arg))
 
     const printExpected = this.utils.printExpected(expected)
-    const printReceived = this.utils.printReceived(received.mock.calls[0])
+    const printReceived = this.utils.printReceived(calls)
 
     return {
-      message: () => `expected ${received.getMockName()} to have been called exactly once with arguments. Expected:\n\n${printExpected}\n\nReceived:\n\n${printReceived}\n`,
-      pass: isCalledOnce && isCalledWithExpected,
+      message: () => `expected ${received.getMockName()} to have been called once with arguments. Expected:\n\n${printExpected}\n\nReceived:\n\n${printReceived}\n`,
+      pass,
     }
   },
 })
