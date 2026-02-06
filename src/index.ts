f154f1b0f6a832ae79689a555e3a69c05620eeae
Patrick Taylor <hello@patricktaylor.dev>
2026-02-06 15:54:40 +0000

    Use `process-supervisor` for automatic graceful shutdown 🛸 (#44)
    Use process-supervisor for automatic graceful shutdown 🛸

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,12 @@
 #!/usr/bin/env node
-import { exitHandler } from './exit-handler'
+import { ProcessSupervisor } from 'process-supervisor'
+
 import { createProgram } from './program'
 
-import type { FSWatcher } from 'chokidar'
-
-let watcher: FSWatcher | undefined
-
-process.on('SIGINT', () => exitHandler(0, 'SIGINT', watcher))
-process.on('SIGTERM', () => exitHandler(0, 'SIGTERM', watcher))
-process.on('uncaughtException', error => exitHandler(1, 'Unexpected Error', watcher, error))
-process.on('unhandledRejection', error => exitHandler(1, 'Unhandled Promise', watcher, error))
+const supervisor = new ProcessSupervisor()
 
 const program = createProgram({
-  setWatcher: (w: FSWatcher) => { watcher = w },
+  supervisor,
 })
 
-program.parseAsync(process.argv).catch(error =>
-  exitHandler(1, 'Program Parse Error', watcher, error)
-)
+program.parseAsync(process.argv)
